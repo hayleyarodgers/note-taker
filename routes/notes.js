547c8db9569063f6f488e4b929733a134fb4544b
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-07-28 09:43:48 +1000

    Write delete route for /api/notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,16 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
-// GET Route for retrieving all the notes
+// GET Route for retrieving all notes
 notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes.`);
 
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new note
+// POST Route for adding a new note
 notes.post('/', (req, res) => {
     console.info(`${req.method} request received to add a note.`);
 
@@ -28,5 +29,27 @@ notes.post('/', (req, res) => {
         res.error('Error adding note.');
     }
 });
+
+// DELETE Route for removing a note
+notes.delete('/:ID', (req, res) => {
+    console.info(`${req.method} request received to delete a note.`);
+
+    const deletedNoteRequest = req.params.ID;
+
+    readFromFile('./db/db.json')
+    .then((data) => {
+        const parsedData = JSON.parse(data);
+        const newDatabase = [];
+        
+        for (let i = 0; i < parsedData.length; i++) {
+            if (deletedNoteRequest !== parsedData[i].note_id) {
+                newDatabase.push(parsedData[i]);
+            }
+        }
+
+        writeToFile('./db/db.json', newDatabase);
+        res.json(`Note deleted successfully.`);
+    });
+})
 
 module.exports = notes;
